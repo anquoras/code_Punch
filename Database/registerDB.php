048487b3129ac69b468829d86f80604eba86f606
anquoras <130691015+anquoras@users.noreply.github.com>
2023-06-08 01:54:28 +0700

    Update registerDB.php

--- Database/registerDB.php
+++ Database/registerDB.php
@@ -1,8 +1,8 @@
 <?php
 
-    $db_server = "157.245.200.191";
-    $db_user = "admin";
-    $db_pass = "7ec7dab4023ede2b34f2445e61b8b47e6ce492c945525daf";
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
     $db_name = "users";
     $connect = "";
 
